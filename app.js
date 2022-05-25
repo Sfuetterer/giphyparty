1e083a8439e62a084c7f914fa91c5cdbe3d9f1d4
Stuze <stephfuetterer@gmail.com>
2022-05-24 20:03:58 -0400

    finished jQuery product

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
 console.log("Let's get this party started!");
 
-const submit = document.querySelector('#submit');
-const div = document.createElement('div')
+const $div = $('#div')
+const search =$("#search")
 
 function addGif(res) {
-      const img = document.createElement('img');
-
       let numResults = res.data.length;
       if (numResults) {
         let randomIdx = Math.floor(Math.random() * numResults);
-        img.src = res.data[randomIdx].images.original.url;
-      }
-      div.append(img)
+        let newGif = $("<img>", {
+            src: res.data[randomIdx].images.original.url,
+          });
+      $div.append(newGif)
+      console.log(randomIdx)
     }
+}
   
-submit.addEventListener("click", async function(e){
+$("form").on("submit", async function(e){
     e.preventDefault();
-    const searchTerm = document.querySelector('#search').value;
+
+    let searchTerm = search.val();
+    search.val("");
+
     const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
         params: {
           q: searchTerm,
           api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
         }
-      })
+      });
       addGif(response.data)
     });
 
     const remove = document.querySelector('#remove')
-    remove.addEventListener("click", function(){
-        div.empty();
+    $('#remove').on("click", function(){
+        $div.empty();
     });
 
 
